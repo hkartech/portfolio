dd5864153f8c7067f526344abc600c72582ae784
Hasnain Khan <hasnainkhangm464@gmail.com>
2026-02-22 16:45:15 +0500

    Rename contactsection to ContactSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Technologies from '@/components/ui/technologies';
 import Footer from '@/components/ui/footer';
 import WhatIDo from '@/components/ui/whatido';
 import MetrixStatics from '@/components/ui/metrixstatics'
-import ContactSection from '@/components/ui/contactsection';
+import ContactSection from '@/components/ui/ContactSection';
 import RecentProjects from '@/components/ui/RecentProjects';
 
 export default function Home() {
-    return (
+    return (    
         <main>
             <RibbonBar />
             <Hero />
